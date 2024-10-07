55b5de46c696b486abb078a2020ea5dd4a7eef27
Erlinguss <D00251914@student.dkit.ie>
2024-10-07 20:58:54 +0100

    Added WeatherData Model Class

---
+++ MoodSync/Models/WeatherData.cs
@@ -0,0 +1,12 @@
+﻿namespace MoodSync.Models
+{
+    public class WeatherData
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double WindSpeed { get; set; }
+    }
+}
