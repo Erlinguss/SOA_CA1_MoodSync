3bed5192b7aeb9cfd9fecf4e27d35aebcc9e2770
Erlinguss <D00251914@student.dkit.ie>
2024-10-07 23:46:14 +0100

    Added Weather Razor page and modified Program Class

---
+++ MoodSync/Components/Services/WeatherService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using MoodSync.Models;
+using Newtonsoft.Json;
+
+
+namespace MoodSync.Components.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "dd991d8d30b152dd4933258672b85628";
+
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<WeatherData> GetWeatherAsync(string location)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={_apiKey}&units=metric";
+       
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+
+                return weatherData;
+            }
+            return null;
+        }
+    }
+}
+
--- MoodSync/Models/WeatherData.cs
+++ MoodSync/Models/WeatherData.cs
@@ -1,12 +1,40 @@
-﻿namespace MoodSync.Models
+﻿using Newtonsoft.Json;
+
+namespace MoodSync.Models
 {
     public class WeatherData
     {
+        [JsonProperty("name")]
         public string City { get; set; }
-        public string Country { get; set; }
-        public string Description { get; set; }
-        public double Temperature { get; set; }
-        public double Humidity { get; set; }
-        public double WindSpeed { get; set; }
+
+        [JsonProperty("main")]
+        public MainData Main { get; set; }
+
+        [JsonProperty("wind")]
+        public WindData Wind { get; set; }
+
+        [JsonProperty("weather")]
+        public WeatherDescription[] Weather { get; set; }
+
+        public class MainData
+        {
+            [JsonProperty("temp")]
+            public double Temperature { get; set; }
+
+            [JsonProperty("humidity")]
+            public int Humidity { get; set; }
+        }
+
+        public class WindData
+        {
+            [JsonProperty("speed")]
+            public double Speed { get; set; }
+        }
+
+        public class WeatherDescription
+        {
+            [JsonProperty("description")]
+            public string Description { get; set; }
+        }
     }
 }
