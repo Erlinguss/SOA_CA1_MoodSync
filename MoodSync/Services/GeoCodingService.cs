e7173065f00a6a239d2510e22c6b8f4497d1aa94
Erlinguss <D00251914@student.dkit.ie>
2024-10-08 23:17:36 +0100

    Implemented GeoCodingService and Modified Weather razor

---
+++ MoodSync/Services/GeoCodingService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using MoodSync.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MoodSync.Services
+{
+    public class GeocodingService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "AIzaSyAARniUO27XBpmFtefiEDN2e9twJs4Tb0U";
+
+        public GeocodingService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<LocationData> GetCoordinatesAsync(string locationName)
+        {
+            var url = $"https://maps.googleapis.com/maps/api/geocode/json?address={locationName}&key={_apiKey}";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var geoCodingData = JsonConvert.DeserializeObject<LocationData>(content);
+                return geoCodingData;
+            }
+            return null;
+        }
+    }
+}
