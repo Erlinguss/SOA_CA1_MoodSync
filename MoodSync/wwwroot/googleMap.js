3d0e42679e9382ab0e137da4bf01a850f606d7f2
Erlinguss <D00251914@student.dkit.ie>
2024-10-09 21:45:30 +0100

    Added googleMap Js

---
+++ MoodSync/wwwroot/googleMap.js
@@ -0,0 +1,18 @@
+﻿var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 53.3498053, lng: -6.2603097 },
+        zoom: 12
+    });
+}
+
+function setMapLocation(latitude, longitude) {
+    var position = { lat: latitude, lng: longitude };
+    map.setCenter(position);
+
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+}
