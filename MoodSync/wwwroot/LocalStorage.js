c45ada27dd1ecb2790c711d25981aa071dc9069e
Erlinguss <D00251914@student.dkit.ie>
2024-10-10 15:51:11 +0100

    Added login and userCredentialData classes

---
+++ MoodSync/wwwroot/LocalStorage.js
@@ -0,0 +1,3 @@
+﻿window.localStorage.getItem = (key) => localStorage.getItem(key);
+window.localStorage.setItem = (key, value) => localStorage.setItem(key, value);
+window.localStorage.removeItem = (key) => localStorage.removeItem(key);
