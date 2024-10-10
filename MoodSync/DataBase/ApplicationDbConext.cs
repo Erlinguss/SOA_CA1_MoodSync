5ad3272cefd5ee993bf6613f0e89c4a35ac56872
Erlinguss <D00251914@student.dkit.ie>
2024-10-10 17:44:55 +0100

    Added ApplicationDbContext class

---
+++ MoodSync/DataBase/ApplicationDbConext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodSync.Models;
+
+namespace MoodSync.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserCredentialsData> Users { get; set; }
+    }
+}
