2c6314b7702ae7dd2050130fd0c268c9287eeb93
Erlinguss <D00251914@student.dkit.ie>
2024-10-10 17:44:55 +0100

    Added ApplicationDbContext class

--- MoodSync/Program.cs
+++ MoodSync/Program.cs
@@ -1,34 +1,57 @@
 using MoodSync.Components;
 using MoodSync.Services;
 using MoodSync.Interfaces;
+using MoodSync.Data;
+using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddScoped<IWeatherService, WeatherService>();
-builder.Services.AddScoped<ILocationService, LocationService>();
-builder.Services.AddScoped<IGeoCodingService, GeoCodingService>();
-builder.Services.AddScoped<RecommendationService>();
-builder.Services.AddScoped<MoodService>();
-
-
-builder.Services.AddHttpClient();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddScoped<IWeatherService, WeatherService>();
+        builder.Services.AddScoped<ILocationService, LocationService>();
+        builder.Services.AddScoped<IGeoCodingService, GeoCodingService>();
+        builder.Services.AddScoped<RecommendationService>();
+        builder.Services.AddScoped<MoodService>();
+
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddAuthentication("Cookies")
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/login";
+                options.LogoutPath = "/logout";
+            });
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddHttpClient();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapDefaultControllerRoute();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
