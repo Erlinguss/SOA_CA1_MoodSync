d25e1322ce9fb002c5d78dca7c1f7e322c4c8460
Erlinguss <D00251914@student.dkit.ie>
2024-10-10 13:26:49 +0100

    Implemented Mood data

---
+++ MoodSync/Models/MoodData.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodSync.Models
+{
+    public class MoodData
+    {
+        public string Mood { get; set; }
+        public string Location { get; set; }
+        public string Timestamp { get; set; } 
+        public List<string> Recommendations { get; set; }
+    }
+}
+
