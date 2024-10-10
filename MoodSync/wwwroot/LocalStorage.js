f38db1a931fd0d228855871097eeb6b0e3caeb9a
Erlinguss <D00251914@student.dkit.ie>
2024-10-10 17:15:40 +0100

    Added MoodSyncDB sql and Updated UserCredentialsData

--- MoodSync/wwwroot/LocalStorage.js
+++
@@ -1,3 +0,0 @@
-﻿window.localStorage.getItem = (key) => localStorage.getItem(key);
-window.localStorage.setItem = (key, value) => localStorage.setItem(key, value);
-window.localStorage.removeItem = (key) => localStorage.removeItem(key);
