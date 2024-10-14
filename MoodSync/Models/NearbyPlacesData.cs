fad8b21ce3fa8b3ce771d1add5c7d3baa63fbb7d
Erlinguss <D00251914@student.dkit.ie>
2024-10-14 18:51:33 +0100

    Implemented professional weather display screen

--- MoodSync/Models/NearbyPlacesData.cs
+++ MoodSync/Models/NearbyPlacesData.cs
@@ -7,9 +7,6 @@ namespace MoodSync.Models
         [JsonProperty("results")]
         public List<Place> Results { get; set; }
 
-        [JsonProperty("html_attributions")]
-        public List<string> HtmlAttributions { get; set; }
-
         public class Place
         {
             [JsonProperty("name")]
@@ -21,31 +18,6 @@ namespace MoodSync.Models
             [JsonProperty("types")]
             public List<string> Types { get; set; }
 
-            [JsonProperty("photos")]
-            public List<Photo> Photos { get; set; }
-
-           /* [JsonProperty("geometry")]
-            public Geometry Geometry { get; set; }*/
-        }
-
-    /*    public class Geometry
-        {
-            [JsonProperty("location")]
-            public LocationCoordinates Location { get; set; }
-        }*/
-
-/*        public class LocationCoordinates
-        {
-            [JsonProperty("lat")]
-            public double Latitude { get; set; }
-
-            [JsonProperty("lng")]
-            public double Longitude { get; set; }
-        }*/
-        public class Photo
-        {
-            [JsonProperty("html_attributions")]
-            public List<string> HtmlAttributions { get; set; }
         }
     }
 }
